2984afacbe3378d546c959e9e168cdfccf886d79
dinhcongthong <dinhcongthong97y@gmail.com>
2022-12-05 23:04:49 +0700

    done on interface

--- resources/views/admin/option/index.blade.php
+++ resources/views/admin/option/index.blade.php
@@ -1,45 +1,62 @@
 @extends('layouts.admin')
+@section('stylesheet')
+    <style>
+        .category-items {
+            border-bottom: 2px solid #0000000d;
+            padding-bottom: 1.5rem;
+        }
+        .category-items:hover {
+            --bs-table-accent-bg: var(--bs-table-striped-bg);
+            color: #727e8c;
+            transform: scale(1.01);
+            transition: 0.2s;
+        }
+        .option-items-bg div.d-block:nth-child(odd) {
+            border: 2px solid #ededed;
+            border-radius: 5px;
+            background-color: #ededed;
+            border-color: #ededed;
+        }
+    </style>
+@endsection
 
 @section('content')
     <div class="w-50 m-auto">
         <section class="section">
             <div class="card">
                 <div class="card-body">
-                    <table class='table' id="option_table">
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($optionCategory as $optionCategory)
-                            <tr>
-                                <td>
-                                    <a class="" data-bs-toggle="collapse"
-                                       role="button"
-                                       href="#category{{ $optionCategory->id }}" aria-expanded="false"
-                                       aria-controls="category{{ $optionCategory->id }}">
-                                        {{ $optionCategory->name }}
-                                    </a>
-                                    <div id="category{{ $optionCategory->id }}" class="collapse"
-                                         aria-labelledby="headingOne">
-                                        <div class="pt-3">
-                                            @foreach ($optionCategory->getOptions as $item)
-                                                <p>{{ $item->name }}</p>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-end">
+                    @foreach ($optionCategory as $optionCategory)
+                        <div class="d-block my-4 category-items">
+                            <div class="d-flex">
+                                <a class="" data-bs-toggle="collapse" role="button"
+                                    href="#category{{ $optionCategory->id }}" aria-expanded="false"
+                                    aria-controls="category{{ $optionCategory->id }}">
+                                    {{ $optionCategory->name }}
+                                </a>
+                                <div class="ml-auto">
                                     <a href="#">
                                         <i data-feather="edit" title="Edit"></i>
                                     </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                                </div>
+                            </div>
+                            <div id="category{{ $optionCategory->id }}" class="collapse" aria-labelledby="headingOne">
+                                <div class="p-3 option-items-bg">
+                                    @foreach ($optionCategory->getOptions as $item)
+                                        <div class="d-block px-2 pt-3">
+                                            <div class="d-flex">
+                                                <p>{{ $item->name }}</p>
+                                                <div class="ml-auto">
+                                                    <a href="#">
+                                                        <i data-feather="edit" title="Edit"></i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </section>
